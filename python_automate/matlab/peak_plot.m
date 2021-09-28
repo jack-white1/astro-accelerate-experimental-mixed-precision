4104f29d60f8521e2d07a04ffcb97ae84d5626ed
Jack White <jackajwhite@gmail.com>
2021-09-28 13:20:40 +0100

    added extra plotting scripts

--- python_automate/matlab/peak_plot.m
+++
@@ -1,7 +0,0 @@
-data_table = readtable('/home/jack/hdd/dat/period_1.25_width_50_snrpeak_4_dm_500_nbits_8_nchans_1024_tsamp_128_tobs_600_fch1_1550_foff_0.292968752_binary_nan_bper_336_bphase_0.2_bpmass_1.5_bcmass_5.0_12-08-2021-23-53-54.248506_bfloat_peaks.dat');
-data_xyz = table2array(data_table);
-data_table = [];
-data_xyz = [data_xyz(:,1) data_xyz(:,4) data_xyz(:,5)];
-dotsize = 50;  %adjust as needed
-scatter3(data_xyz(:,1), data_xyz(:,2), data_xyz(:,3), dotsize, data_xyz(:,3), 'filled');
-colormap turbo
--- python_automate/supervisor.py
+++ python_automate/supervisor.py
@@ -9,6 +9,19 @@ import itertools
 import json
 from datetime import datetime
 import time
+import random
+import string
+import shutil
+
+def round_trailing_zeros(targetDict):
+	roundedDict = {}
+	for key in targetDict:
+		if targetDict[key] % 1 == 0:
+			roundedDict[key] = int(targetDict[key])
+		else:
+			roundedDict[key] = targetDict[key]
+	return roundedDict
+
 
 def is_non_zero_file(filepath):
 	return (os.path.isfile(filepath) and (os.path.getsize(filepath) > 0))
@@ -41,7 +54,8 @@ def returnLastLine(filepath):
 def makeRandomFakeParameters(parameterBounds = {}):
 	nsamps = 1
 	if parameterBounds == {}:
-		parameterBounds = {	'period': [1.25, 8000],\
+		parameterBounds = {'seed': [0,1],\
+					'period': [1.25, 8000],\
 					'width':[4,50],\
 					'snrpeak':[0.0125, 0.125],\
 					'dm':[5,500],\
@@ -62,19 +76,61 @@ def makeRandomFakeParameters(parameterBounds = {}):
 		try:
 			lb = parameterBounds[key][0]
 			ub = parameterBounds[key][1]
-			if key == "dm":
+			if key == "seed":
+				fakeParameters[key] = random.randint(0,2147483647)
+			elif key == "dm":
 				fakeParameters[key] = np.random.randint(lb,ub,nsamps).tolist()[0]
 			else:
 				fakeParameters[key] = [round(elem, 5) for elem in np.random.uniform(lb,ub,nsamps).tolist()][0]
 		except:
 			fakeParameters[key] = [parameterBounds[key][0]][0]
 			print(key + " is not a variable parameter")
+
+	return fakeParameters
+
+def makeLogUniformFakeParameters(parameterBounds = {}):
+	nsamps = 1
+	if parameterBounds == {}:
+		parameterBounds = {'seed': [0,1],\
+					'period': [1.25, 1000],\
+					'width':[4,50],\
+					'snrpeak':[0.0125, 0.125],\
+					'dm':[5,500],\
+					'nbits':[8],\
+					'nchans':[1024],\
+					'tsamp':[128],\
+					'tobs':[600],\
+					'fch1':[1550],\
+					'foff':[0.292968752],\
+					'binary':[float('nan')],\
+					'bper':[1.5, 336],\
+					'bphase':[0.2],\
+					'bpmass':[1.0, 1.5],\
+					'bcmass':[0.1, 5.0]}
+	fakeParameters = {}
+	base = 2
+	for key in parameterBounds:
+		if len(parameterBounds[key]) == 2:
+			if key == "seed":
+				fakeParameters[key] = random.randint(0,2147483647)
+			elif key == "dm":
+				lb = math.log(parameterBounds[key][0],base)
+				ub = math.log(parameterBounds[key][1],base)
+				fakeParameters[key] = [int(round(base**elem, 5)) for elem in np.random.uniform(lb,ub,nsamps).tolist()][0]
+			else:
+				lb = math.log(parameterBounds[key][0],base)
+				ub = math.log(parameterBounds[key][1],base)
+				fakeParameters[key] = [round(base**elem, 5) for elem in np.random.uniform(lb,ub,nsamps).tolist()][0]
+		elif len(parameterBounds[key]) == 1:
+			fakeParameters[key] = [parameterBounds[key][0]][0]
+#				print(key + " is not a variable parameter")
 	return fakeParameters
 
 def make1DFakeParameters(axis):
 	nsamps = 25
 
-	parameterBounds = {	'period': [1.25, 8000],\
+	parameterBounds = {'seed' : [0,1],\
+					'period': [1.25, 8000],\
 					'width':[4,50],\
 					'snrpeak':[0.0125, 0.125],\
 					'dm':[5,500],\
@@ -90,10 +146,11 @@ def make1DFakeParameters(axis):
 					'bpmass':[1.0, 1.5],\
 					'bcmass':[0.1, 5.0]}
 
-	defaultParameters = {'period': [10],\
-					'width':[25],\
-					'snrpeak':[0.25],\
-					'dm':[100],\
+	defaultParameters = {'seed' : [random.randint(0,2147483647)],\
+					'period': [22.9],\
+					'width':[47],\
+					'snrpeak':[1],\
+					'dm':[50],\
 					'nbits':[8],\
 					'nchans':[1024],\
 					'tsamp':[128],\
@@ -101,10 +158,10 @@ def make1DFakeParameters(axis):
 					'fch1':[1550],\
 					'foff':[0.292968752],\
 					'binary':[float('nan')],\
-					'bper':[15],\
+					'bper':[2.45],\
 					'bphase':[0.2],\
-					'bpmass':[1.25],\
-					'bcmass':[2.0]}
+					'bpmass':[1.338],\
+					'bcmass':[1.249]}
 
 	fakeParameters = defaultParameters
 	try:
@@ -119,6 +176,61 @@ def make1DFakeParameters(axis):
 		print(axis + " is not a variable parameter")
 
 	return fakeParameters
+
+def make1DLinSpaceFakeParameters(axis):
+	nsamps = 300
+
+#	parameterBounds = {'seed' : [0,1],\
+	parameterBounds = {'seed' : [1970963475],\
+					'period': [22.89843, 22.9],\
+					'width':[4,50],\
+					'snrpeak':[0.0125, 0.125],\
+					'dm':[5,500],\
+					'nbits':[8],\
+					'nchans':[1024],\
+					'tsamp':[128],\
+					'tobs':[600],\
+					'fch1':[1550],\
+					'foff':[0.292968752],\
+					'binary':[float('nan')],\
+					'bper':[1.5, 336],\
+					'bphase':[0.51, 0.54],\
+					'bpmass':[1.0, 1.5],\
+					'bcmass':[0.1, 5.0]}
+
+#	defaultParameters = {'seed' : [random.randint(0,2147483647)],\
+	defaultParameters = {'seed' : [1970963475],\
+					'period': [22.9],\
+					'width':[47],\
+					'snrpeak':[1],\
+					'dm':[50],\
+					'nbits':[8],\
+					'nchans':[1024],\
+					'tsamp':[128],\
+					'tobs':[600],\
+					'fch1':[1550],\
+					'foff':[0.292968752],\
+					'binary':[float('nan')],\
+					'bper':[2.45],\
+					'bphase':[0.2],\
+					'bpmass':[1.338],\
+					'bcmass':[1.249]}
+
+	fakeParameters = defaultParameters
+	try:
+		lb = parameterBounds[axis][0]
+		ub = parameterBounds[axis][1]
+		if axis == "dm":
+			fakeParameters[axis] = [round(elem, 0) for elem in np.linspace(lb,ub,nsamps).tolist()]
+		else:
+			fakeParameters[axis] = [round(elem, 8) for elem in np.linspace(lb,ub,nsamps).tolist()]
+	except:
+		fakeParameters[axis] = [parameterBounds[axis][0]]
+		print(axis + " is not a variable parameter")
+
+	#print(fakeParameters)
+	return fakeParameters
+
 
 def makeFakeToDoList(fakeParameters):
 	keys, values = zip(*fakeParameters.items())
@@ -152,6 +264,7 @@ def makeFilFile(parameters):
 	filFileName ='/home/jack/hdd/fil/' +filFileName+ '.fil'
 	fakeArguments +=(' > ' + filFileName)
 	fakeExecutable = '/home/jack/Documents/mk_sigproc/executables/bin/fake ' + fakeArguments
+	#print(fakeExecutable)
 	os.system(fakeExecutable)
 	filepath_parameters = {'filepath':filFileName, 'parameters':parameters}
 	with open("aaToDoList.txt", "a") as f:
@@ -176,8 +289,8 @@ def makeDatFile(filepath, parameters, precision):
 
 	aaTargetDM = str(parameters['dm'])
 
-	minDM = max(1,int(aaTargetDM)-50)
-	maxDM = int(aaTargetDM) + 50
+	minDM = max(1,int(float(aaTargetDM))-50)
+	maxDM = int(float(aaTargetDM)) + 50
 
 	with open(standardInputFilePath, 'r') as file:
 			inputFileText = file.read()
@@ -190,71 +303,59 @@ def makeDatFile(filepath, parameters, precision):
 	with open(tempInputFilePath, "w+") as text_file:
 		text_file.write(inputFileText)
 
-	aaExecutable = aaPath[precision]+' '+ tempInputFilePath + ' ' + aaTargetDM + ' ' + aaOutputDatFileName# + ' >/dev/null 2>&1'
+	aaExecutable = aaPath[precision]+' '+ tempInputFilePath + ' ' + aaTargetDM + ' ' + aaOutputDatFileName #+ ' >/dev/null 2>&1'
 	print("Running AA...")
-	#subprocess.run(['astro-accelerate', tempInputFilePath, aaTargetDM, aaOutputDatFileName], shell=True, cwd=aaPath[precision])
 	print(aaExecutable)
 	os.system(aaExecutable)
 	print("Finished AA...")
 	aaOutputDatFileName += "peaks.dat"
 	print(aaOutputDatFileName)
-	#result = subprocess.run(['ls', '-t', '/home/jack/hdd/dat'], capture_output=True)
-
-	#datFilePath = './dat/'+result.stdout.decode("utf-8").partition('\n')[0].split()[0]
-#	peaks = []
-#	with open(aaOutputDatFileName, newline='') as csvfile:
-#		datreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
-#		for row in datreader:
-#			peaks.append([float(row[0]),float(row[3]),float(row[5])]) #acceleration, frequency, snr
 
 
-	#print(data)
-	#data = data.sort(key = lambda x: x[2])
-	#print(sorted(data, key = lambda x: x[2]))
-	#data = np.asarray(sorted(data, key = lambda x: x[2]))
-#	peaks = sorted(peaks, key = lambda x: x[2], reverse=True)
-	#print(peaks[0:100])
+def makePrestoDatFile(filepath, parameters, precision):
 
-#	max_freq = sorted(peaks, key = lambda x: x[1], reverse=True)[0][1]
-#	print("max_freq: " + str(max_freq))
+	rand_str_length = 25
+	prestoTargetDM = 50
+	prestoNumharm = 1
+	prestoZmax = 200
+	rand_str = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(rand_str_length))
+	#make symlink to fil file
+	targetFilFile = filepath
+	alias = "/home/jack/hdd/symlinks/" + rand_str + ".fil"
+	os.symlink(filepath, alias)
 
-	#max_result = data[np.argmax(data[:,2]),:].tolist()
+	prepsubbandExecutable = '/home/jack/Documents/PRESTO/presto/bin/prepsubband -o ' + rand_str + ' -nobary '+ alias + ' -lodm '+str(prestoTargetDM)+' -dmstep 1 -numdms 1'
+	print(prepsubbandExecutable)
+	os.system(prepsubbandExecutable)
+	shutil.move('/home/jack/Documents/aa_mixed/astro-accelerate-experimental-mixed-precision/python_automate/' + rand_str + "_DM" +str(prestoTargetDM)+ ".00.dat",'/home/jack/hdd/presto/' + rand_str + ".dat")
+	shutil.move('/home/jack/Documents/aa_mixed/astro-accelerate-experimental-mixed-precision/python_automate/' + rand_str + "_DM" +str(prestoTargetDM)+ ".00.inf",'/home/jack/hdd/presto/' + rand_str + ".inf")
+	
+	accelsearchExecutable = '/home/jack/Documents/PRESTO/presto/bin/accelsearch -flo 0 -fhi 800 -zmax '+str(prestoZmax)+' -wmax 0 -numharm '+str(prestoNumharm)+' /home/jack/hdd/presto/' + rand_str + ".dat"
+	print(accelsearchExecutable)
+	os.system(accelsearchExecutable)
 
-#	max_result = peaks[0]
-#	print("peak at " + str(max_result))
+	accelsearchOutput = '/home/jack/hdd/presto/' +rand_str + "_ACCEL_" + str(prestoZmax)
+	accelsearchMetaOutput = ''
+	for key in parameters:
+		accelsearchMetaOutput += key + '_' + str(parameters[key]) + '_'
+	accelsearchMetaOutput += filepath[-31:-5] + '_PRESTO_peaks.dat'
+	print("filepath: ")
+	print(filepath)
+	print("parameters: ")
+	print(parameters)
+	print("precision: ")
+	print(precision)
+	print("accelsearchOutput: " + accelsearchOutput)
+	print("accelsearchMetaOutput: " + accelsearchMetaOutput)
+	shutil.move(accelsearchOutput, '/home/jack/hdd/presto/' + accelsearchMetaOutput)
+	os.remove('/home/jack/hdd/presto/' + rand_str + ".dat")
+	os.remove('/home/jack/hdd/presto/' + rand_str + ".inf")
+	os.remove('/home/jack/hdd/presto/' + rand_str + "_ACCEL_" + str(prestoZmax) + ".cand")
+	#prepsubband with 100char random filename output and symlink input
 
-#	period = parameters['period']*0.001		# convert to ms
-#	fundamental_freq = 1/period				# Hz
-#	print("fundamental_freq at : " + str(fundamental_freq) + " Hz" )
-
-#	nBands = math.floor(max_freq/fundamental_freq)
-
-#	bandBoundaries = []
-
-#	print("nBands = " + str(nBands))
-#	for i in range(nBands):
-#		bandBoundaries.append([fundamental_freq*(i+0.9), fundamental_freq*(i+1.1)])
-#	harmonics = []
-
-#	for band in range(nBands):
-#		#print("Harmonic band "+str(band)+" from "+ str(bandBoundaries[band][0]) +" Hz to "+ str(bandBoundaries[band][1]) +" Hz")
-#		candidate_peak = [0,0,0]
-#		for peak in peaks:
-#			if ((peak[1] > bandBoundaries[band][0]) and (peak[1] < bandBoundaries[band][1]) and peak[2] > candidate_peak[2]):
-#				candidate_peak = peak
-#		harmonics.append(candidate_peak)
-
-	#print("Harmonics:")
-#	i = 0
-#	for harmonic in harmonics:
-		#print("Max peak in band "+str(i)+", [acc,f,pow] = " +str(harmonic) + " which is " + str(harmonic[1]/fundamental_freq) + "x the fundamental freq")
-#		i+=1
-
-#	results = {"peak_list":json.dumps(harmonics),'precision':precision,"filFilePath":filepath,"aaOutputDatFileName":aaOutputDatFileName}
-#	results.update(parameters)
-#	with open("results.txt", "a") as f:
-#		f.write(json.dumps(results)+"\n")
-
+	#accelsearch with 100char random filename input, move it to /hdd/presto/original_fil_filename
+	#delete all intermediate presto files
+	#os.unlink()
 
 
 if __name__ == "__main__":
@@ -263,14 +364,18 @@ if __name__ == "__main__":
 #	print(fakeParameters)
 #	makeFakeToDoList(fakeParameters)
 
-	randomSamples = 1
+#	fakeParameters = make1DLinSpaceFakeParameters("bphase")
+#	print(fakeParameters)
+#	makeFakeToDoList(fakeParameters)
+
+
+	randomSamples = 200
 	fakeParameters = []
 	for i in range(randomSamples):
-		fakeParameters.append(makeRandomFakeParameters())
+		fakeParameters.append(makeLogUniformFakeParameters())
 	print(fakeParameters)
 	makeRandomFakeToDoList(fakeParameters)
-
-
+#
 	numthreads = 12
 
 	while is_non_zero_file("fakeToDoList.txt"):
@@ -288,21 +393,43 @@ if __name__ == "__main__":
 		with mp.Pool(numthreads) as p:
 			p.map(makeFilFile, params_map)
 
-#	with open("aaToDoList.txt", "a") as f:
-#		f.write('{"filepath": "/home/jack/hdd/fil/period_22.9_width_50.0_snrpeak_5_dm_50_nbits_8_nchans_1024_tsamp_128_tobs_600_fch1_1550_foff_0.292968752_binary_nan_bper_2.45_bphase_0.1_bpmass_1.338_bcmass_1.249_09-08-2021-11-21-26.349980_.fil", "parameters": {"period": 22.9, "width": 50.0, "snrpeak": 5, "dm": 50, "nbits": 8, "nchans": 1024, "tsamp": 128, "tobs": 600, "fch1": 1550, "foff": 0.292968752, "binary": NaN, "bper": 2.45, "bphase": 0.1, "bpmass": 1.338, "bcmass": 1.249}}')
-#		f.write('{"filepath": "/home/jack/hdd/fil/period_616.6_width_25_snrpeak_0.25_dm_100_nbits_8_nchans_1024_tsamp_128_tobs_600_fch1_1550_foff_0.292968752_binary_nan_bper_15_bphase_0.2_bpmass_1.25_bcmass_2.0_17-08-2021-13-45-15.719242_.fil", "parameters": {"period": 616.6, "width": 25.0, "snrpeak": 0.25, "dm": 100, "nbits": 8, "nchans": 1024, "tsamp": 128, "tobs": 600, "fch1": 1550, "foff": 0.292968752, "binary": NaN, "bper": 15, "bphase": 0.2, "bpmass": 1.25, "bcmass": 2.0}}')
-
 
 	while is_non_zero_file("aaToDoList.txt"):
 		last_line = returnLastLine("aaToDoList.txt")
 		deleteLastLine("aaToDoList.txt")
 		filepath_parameters = json.loads(last_line)
 		filepath = filepath_parameters['filepath']
-		parameters = filepath_parameters['parameters']
+		parameters = round_trailing_zeros(filepath_parameters['parameters'])
+		print(parameters)
 		makeDatFile(filepath, parameters, 'bfloat')
 		makeDatFile(filepath, parameters, 'single')
 		makeDatFile(filepath, parameters, 'double')
-#
+#		makePrestoDatFile(filepath, parameters, 'presto')
+		if os.path.exists(filepath):
+			os.remove(filepath)
+		else:
+			print(filepath + " does not exist")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #	os.system("python3 graph_generators/dat2results.py")
 
 #
@@ -316,51 +443,3 @@ if __name__ == "__main__":
 #	else:
 #		print("fakeToDoList.txt does not exist")
 #
-#	fakeParameters = {	'period': [22.9],\
-#						'width':[50.0],\
-#						'snrpeak':[1],\
-#						'dm':[50],\
-#						'nbits':[8],\
-#						'nchans':[1024],\
-#						'tsamp':[128],\
-#						'tobs':[600],\
-#						'fch1':[1550],\
-#						'foff':[0.292968752],\
-#						'binary':[float('nan')],\
-#						'bper':[2.45],\
-#						'bphase':[0.1],\
-#						'bpmass':[1.338],\
-#						'bcmass':[1.249]}
-
-#	fakeParameters = {	'period': [22.9],\
-#						'width':[50.0],\
-#						'snrpeak':[5],\
-#						'dm':[50],\
-#						'nbits':[8],\
-#						'nchans':[1024],\
-#						'tsamp':[128],\
-#						'tobs':[600],\
-#						'fch1':[1550],\
-#						'foff':[0.292968752]}
-
-
-#	fakeParameters = {	'period': [1.25, 8],\
-#						'width':[4.0, 50.0],\
-#						'snrpeak':[0.025, 1],\
-#						'dm':[5, 500],\
-#						'nbits':[8],\
-#						'nchans':[1024],\
-#						'tsamp':[128],\
-#						'tobs':[600],\
-#						'fch1':[1550],\
-#						'foff':[0.292968752],\
-#						'binary':[float('nan')],\
-#						'bper':[1.5, 336],\
-#						'bphase':[0.0,0.025, 0.05, 0.075, 0.1,0.125,0.15,0.175,0.2,0.225,0.25,0.275,0.3,0.325,0.35,0.375,0.4,0.425,0.45,0.475,0.5,0.525,0.55,0.575,0.6,0.625,0.65,0.675,0.7,0.725,0.75,0.775,0.8,0.825,0.85,0.875,0.9,0.925,0.95,0.975,1.0],\
-#						'bpmass':[1.0, 1.35, 1.5],\
-#						'bcmass':[0.1, 0.25, 5.0]}
-	#each thread takes 10 mins, then 576 overnight
-
-	#
-#	with open("aaToDoList.txt", "a") as f:
-#		f.write('{"filepath": "/home/jack/hdd/fil/period_22.9_width_50.0_snrpeak_5_dm_50_nbits_8_nchans_1024_tsamp_128_tobs_600_fch1_1550_foff_0.292968752_binary_nan_bper_2.45_bphase_0.1_bpmass_1.338_bcmass_1.249_09-08-2021-11-21-26.349980_.fil", "parameters": {"period": 22.9, "width": 50.0, "snrpeak": 5, "dm": 50, "nbits": 8, "nchans": 1024, "tsamp": 128, "tobs": 600, "fch1": 1550, "foff": 0.292968752, "binary": NaN, "bper": 2.45, "bphase": 0.1, "bpmass": 1.338, "bcmass": 1.249}}')
